cc8fcf50962ef21474dd3504f7b7b0e4e7c72071
Florent Yvon <florent.yvon76@gmail.com>
2026-04-28 11:59:33 +0100

    Using pydantic for env variables

--- config.py
+++ config.py
@@ -1,21 +1,27 @@
 import os
 
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 def env_bool(var_name: str, default: bool = False) -> bool:
     """Helper function to read boolean environment variables."""
     return os.environ.get(var_name, str(default)).lower() in ("true", "1", "yes")
 
 
-class Config:
-    SECURED = env_bool("SECURED", default=True)
-    PUBLIC_KEY_FILE = os.environ.get("PUBLIC_KEY_FILE", "public.pem")
-    JWT_EXPECTED_ISSUER = os.environ.get("JWT_EXPECTED_ISSUER", "gwas-deposition-app")
-    JWT_EXPECTED_AUDIENCE = os.environ.get("JWT_EXPECTED_AUDIENCE", "pgs-deposition-api")
+class Config(BaseSettings):
+    SECURED: bool = True
+    PUBLIC_KEY_FILE: str = "public.pem"
+    JWT_EXPECTED_ISSUER: str = "gwas-deposition-app"
+    JWT_EXPECTED_AUDIENCE: str = "pgs-deposition-api"
     # File size limit set to 4MB (average is 400-500K)
     MAX_CONTENT_LENGTH = os.environ.get("MAX_CONTENT_LENGTH", 4 * 1000 * 1000)
     DEBUG = env_bool("DEBUG", default=False)
+    MAX_CONTENT_LENGTH: int = 4 * 1000 * 1000
+    DEBUG: bool = False
+
 
 
 class TestConfig(Config):
-    SECURED = False
-    DEBUG = True
+    SECURED: bool = False
+    DEBUG: bool = True
